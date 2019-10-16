1453464a3a1158f2f27ec0523691064b0f3a6015
Ivica Ivankovic <43587818+csivca@users.noreply.github.com>
2019-10-16 18:15:05 +0200

    Add docker compose env

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 function ReplaceConf { 
-    sed -i "s/pr0xysql/$PROXYSQL_PASSWORD/" /etc/proxysql/proxysql.cnf
-    sed -i "s/proxysqlpassword/$MONITORING_PASSWORD/" /etc/proxysql/proxysql.cnf
     sed -i "s/db1.cluster.node/$CLUSTER_IP1/" /etc/proxysql/proxysql.cnf
     sed -i "s/db2.cluster.node/$CLUSTER_IP2/" /etc/proxysql/proxysql.cnf
     sed -i "s/db3.cluster.node/$CLUSTER_IP3/" /etc/proxysql/proxysql.cnf
-    sed -i "s/passw0rd/$PROXYSQL_USER/" /etc/proxysql/proxysql.cnf
+    sed -i "s/pr0xyPass/$PROXYSQL_PASSWD/" /etc/proxysql/proxysql.cnf
+    sed -i "s/adminWebMON/$WEBMON_PASSWD/" /etc/proxysql/proxysql.cnf
+    sed -i "s/pr0xysql/$PSQLDBS_USER/" /etc/proxysql/proxysql.cnf
+    sed -i "s/passw0rd/$PSQLDBS_PASSWD/" /etc/proxysql/proxysql.cnf
     
 }
 
 function StartProxy { 
-    /etc/init.d/proxysql
+    exec proxysql -f /etc/proxysql/proxysql.cnf
 }
 
 ReplaceConf
