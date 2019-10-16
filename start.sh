58c96dd59a1213ee431a1464935d6868a4ab1316
Ivica Ivankovic <43587818+csivca@users.noreply.github.com>
2019-10-16 18:30:19 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function ReplaceConf { 
-    sed -i "s/db1.cluster.node/$CLUSTER_IP1/" /etc/proxysql/proxysql.cnf
-    sed -i "s/db2.cluster.node/$CLUSTER_IP2/" /etc/proxysql/proxysql.cnf
-    sed -i "s/db3.cluster.node/$CLUSTER_IP3/" /etc/proxysql/proxysql.cnf
-    sed -i "s/pr0xyPass/$PROXYSQL_PASSWD/" /etc/proxysql/proxysql.cnf
-    sed -i "s/adminWebMON/$WEBMON_PASSWD/" /etc/proxysql/proxysql.cnf
-    sed -i "s/pr0xysql/$PSQLDBS_USER/" /etc/proxysql/proxysql.cnf
-    sed -i "s/passw0rd/$PSQLDBS_PASSWD/" /etc/proxysql/proxysql.cnf
+    sed -i "s/db1.cluster.node/$CLUSTER_IP1/" /etc/proxysql.cnf
+    sed -i "s/db2.cluster.node/$CLUSTER_IP2/" /etc/proxysql.cnf
+    sed -i "s/db3.cluster.node/$CLUSTER_IP3/" /etc/proxysql.cnf
+    sed -i "s/pr0xyPass/$PROXYSQL_PASSWD/" /etc/proxysql.cnf
+    sed -i "s/adminWebMON/$WEBMON_PASSWD/" /etc/proxysql.cnf
+    sed -i "s/pr0xysql/$PSQLDBS_USER/" /etc/proxysql.cnf
+    sed -i "s/passw0rd/$PSQLDBS_PASSWD/" /etc/proxysql.cnf
     
 }
 
